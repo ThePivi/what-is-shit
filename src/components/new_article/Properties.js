76e3b7ca1d180f27d30a73ba3085e663198d6b92
ThePivi <47472659+ThePivi@users.noreply.github.com>
2022-10-26 21:42:56 +0200

    Some of basic controlls implemented on properties.js

--- src/components/new_article/Properties.js
+++ src/components/new_article/Properties.js
@@ -17,6 +17,22 @@ function Properties () {
                 <label className="form-label">Értékelések:</label> <br />
                 <label htmlFor="Grafika" className="form-label">Grafika</label>
                 <input type="range" className="form-range" id="Grafika"></input>
+                <label htmlFor="Zene" className="form-label">Zene</label>
+                <input type="range" className="form-range" id="Zene"></input>
+                <label htmlFor="Hangok" className="form-label">Hangok</label>
+                <input type="range" className="form-range" id="Hangok"></input>
+                <label htmlFor="Szinkron" className="form-label">Szinkron</label>
+                <input type="range" className="form-range" id="Szinkron"></input>
+                <label htmlFor="Történet" className="form-label">Történet</label>
+                <input type="range" className="form-range" id="Történet"></input>
+                <label htmlFor="Irányítás" className="form-label">Irányítás</label>
+                <input type="range" className="form-range" id="Irányítás"></input>
+                <label htmlFor="Újrajátszhatóság" className="form-label">Újrajátszhatóság</label>
+                <input type="range" className="form-range" id="Újrajátszhatóság"></input>
+            </div>
+            <div className="form-check form-switch">
+                <label htmlFor="Online required" label class="form-check-label">Online required</label>
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
             </div>
             <div>
                 <label>ClickBait:</label>
