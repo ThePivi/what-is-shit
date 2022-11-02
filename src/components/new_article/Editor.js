221b1ef4d233796e3bffc5c8d67ea21b678b0c56
Rakoczik <rakoczi.k21@gmail.com>
2022-11-02 21:36:41 +0100

    Rich text editor feature 1.0

--- src/components/new_article/Editor.js
+++ src/components/new_article/Editor.js
@@ -1,9 +1,31 @@
-function Editor () {
+import React, { useState } from 'react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import parse from 'html-react-parser';
+
+import "./Editor.css";
+
+
+function Editor() {
+    const [text, setText] = useState("")
     return (
         <div className="col-lg-8 offset-lg-0">
             <label>Editor</label>
-            <textarea class="form-control" id="exampleTextarea" rows="20"></textarea>
-        </div>
+            <div className="editor">
+                <CKEditor
+                    className="editor2"
+                    editor={ClassicEditor}
+                    data={text}
+                    onChange={(event, editor) => {
+                        const data = editor.getData()
+                        setText(data)
+                    }} />
+            </div>
+            <div>
+                <h2>Content</h2>
+                <p>{parse(text)}</p>
+            </div>
+        </div >
     );
 }
 
