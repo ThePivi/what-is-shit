b51f6d01cb2a1d6548a0a4bcd143d322947e5d38
ThePivi <47472659+ThePivi@users.noreply.github.com>
2022-11-30 20:17:14 +0100

    profile data
    Creating profile data structure basics

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,24 @@
 import CardController from "../components/layout/CardController";
+import PlanToRead from "./PlanToRead";
+
+const profileData = {
+    id:0,
+    email: "pivarcsik.peter@gmail.com",
+    authorization:"ADMIN",
+    name:"Admiral Kunkka",
+    profilePicture:'https://scontent-nrt1-1.xx.fbcdn.net/v/t1.6435-9/118343719_1758645397608074_5920063687015986170_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=nK2z7ttVorsAX9-huTi&_nc_ht=scontent-nrt1-1.xx&oh=00_AfB_2xh-jR3_Z06084BQGYrbhEEy37s8ox4ehG0JFe0r9w&oe=63A19AAF',
+    layoutConfiguration: {margin:4,columns:3},
+    achivementList:[0,2,33,41],
+    ratingsList:[{articleId:0,rating:90},{articleId:11,rating:30}],
+    planToReadList:[1,2,3,4,5,6,7,8,9,10]
+}
 
 function ProfilePage() {
     return (
         <CardController>
             <dev>
                 <div className="form-group">
-                    <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
+                    <label for="exampleInputEmail1" className="form-label mt-4">Email address: </label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
